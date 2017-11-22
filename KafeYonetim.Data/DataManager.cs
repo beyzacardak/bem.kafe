1b5eaca5256f97252c6b6d1f72c1273f632fb319
beyzacardak <beyzaaa.cardak@gmail.com>
2017-11-22 23:58:13 +0300

    .

--- KafeYonetim.Data/DataManager.cs
+++ KafeYonetim.Data/DataManager.cs
@@ -8,7 +8,7 @@ namespace KafeYonetim.Data
 
     public class DataManager
     {
-        private static string connStr = "Data Source=DESKTOP-S3O5AOR;Initial Catalog=KafeYonetim;Integrated Security=True";
+        private static string connStr = "Data Source=LENOVO\\SQLEXPRESS;Initial Catalog=KafeYonetim;Integrated Security=True";
 
         private static SqlConnection CreateConnection()
         {
